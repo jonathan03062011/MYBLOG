36a3cde6dc1cb50da0558a4334904324cbe7ae57
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-04-15 11:44:15 +0530

    Add files via upload

---
+++ project102,103/main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+    width:360,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function takesnapshot()
+{
+  Webcam.snap(function(data_uri){
+      document.getElementById("output").innerHTML="<img id='capturedimage' src='"+data_uri+"'>";
+  });
+}
+
+console.log('ml5 version:',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yfXWA7mn5/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("model loaded");
+}
